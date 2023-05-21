e205dc6f8af1bf48e4c5dd8fa6533e3e97022407
Vadim <39588115+vnikifirov@users.noreply.github.com>
2023-05-21 15:43:39 +0300

    Upload ConverterPi project

---
+++ ConverterPi/app/src/main/java/com/nikifovadim/converterpi/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.nikifovadim.converterpi
+
+import android.content.res.ColorStateList
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        var output = findViewById<TextView>(R.id.userOutput)
+        var userInput = findViewById<TextView>(R.id.userInput)
+        var btn_Button = findViewById<Button>(R.id.button)
+
+        btn_Button.setOnClickListener {
+            var userText = userInput.text.toString().toLowerCase()
+            var pi = Math.PI.toString().toLowerCase().subSequence(0, 4)
+            if (pi.equals(userText))
+            {
+                var reversedInput = userText.reversed()
+                output.text = reversedInput
+            } else {
+                output.text = "Incorrect number PI"
+                output.setTextColor(getResources().getColor(com.google.android.material.R.color.design_error))
+            }
+        }
+    }
+}
